9a80b0c973ab260f42b7adf2121518989562b39a
Command_String <rsnedeker20@gmail.com>
2023-11-06 04:59:28 -0500

    Created Generic Packet abstraction

---
+++ src/Server/Packets/GenericPacket.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace CavePHP\Server\Packets;
+
+use CavePHP\Server\Client;
+
+class GenericPacket
+{
+    private readonly int $length;
+    private readonly PacketType $type;
+    protected readonly ByteStream $stream;
+
+    public function __construct(
+        public readonly Client $client
+    )
+    {
+        $this->stream = new ByteStream;
+    }
+
+    public function getLength(): int
+    {
+        return $this->length ??=
+            ByteStream::createWithData(
+                $this->stream->getData()
+            )->readVarInt();
+    }
+
+    public function getType(): PacketType
+    {
+        return $this->type ??=
+            PacketType::createFromIdAndStatus(
+                ByteStream::createWithData(
+                    $this->stream->getData()->getRange(3)
+                )->readVarInt(),
+                $this->client->getState()
+            );
+    }
+
+    public function write(string $data): void
+    {
+        $this->stream->write($data);
+    }
+
+    public function isReady(): bool
+    {
+        var_dump((string)$this->stream->getData());
+
+        return $this->getLength() === $this->length;
+    }
+}
