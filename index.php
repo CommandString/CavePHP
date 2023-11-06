6495ce53c43654c1f258e90c9c5b57718ef5822c
Command_String <rsnedeker20@gmail.com>
2023-11-06 05:00:17 -0500

    Example?
    Server starts but when you connect all it does is var_dump a generic packet type with the non decoded binary above it. Eventually this will do more than just timeout

--- index.php
+++ index.php
@@ -1,14 +1,42 @@
 <?php
 
-use CavePHP\Server\Logger\Level;
+use CavePHP\Server\Client;
+use CavePHP\Server\Logger\Color;
+use CavePHP\Server\Packets\GenericPacket;
+use CavePHP\Server\Packets\Uniq\Handshaking\Handshake;
 use CavePHP\Server\Server;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$server = new Server();
+$server = new Server('127.0.0.1');
 
 $server->on(Server::READY, function () use ($server) {
-    $server->logger->log(Level::INFO, 'Server is ready!');
+    $server->logger->info(bold('READY', Color::GREEN));
+});
+
+$server->on(Server::ERROR, function (Throwable $e) use ($server) {
+    $server->logger->error('Server has errored!');
+    echo $e->getMessage() . PHP_EOL;
+});
+
+$server->on(Server::CLOSE, function (Client $client, Server $server) {
+    $server->logger->info('Server has closed');
+});
+
+$server->on(Server::NEW_CLIENT, function (Client $client, Server $server) {
+    $server->logger->info('New client handshake starting [' . color(Color::GREEN, $client->getIp()) . ']');
+
+    $client->on(Client::GENERIC_PACKET, static function (GenericPacket $packet, Client $client) {
+        var_dump($packet->getType());
+    });
+    
+    $client->on(Client::DISCONNECT, static function (Client $client) {
+        $client->server->logger->info('Client disconnected [' . color(Color::GREEN, $client->getIp()) . ']');
+    });
+});
+
+$server->on(Handshake::class, static function (Handshake $packet, Client $client): void {
+    echo 'Handshake packet received' . PHP_EOL;
 });
 
 $server->start();
