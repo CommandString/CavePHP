7c2ea6ae01df90e932d17ae6372cfdf606b4c813
Command_String <rsnedeker20@gmail.com>
2023-11-06 05:01:04 -0500

    Refactor

--- src/Server/Client.php
+++ src/Server/Client.php
@@ -2,16 +2,25 @@
 
 namespace CavePHP\Server;
 
+use CavePHP\Server\Packets\ClientState;
+use CavePHP\Server\Packets\GenericPacket;
 use CavePHP\Utilities\EventEmitter;
 use React\Socket\ConnectionInterface;
 
+/**
+ * @property $username
+ */
 class Client extends EventEmitter
 {
     public const RAW_DATA = 'raw_data';
-    public const PACKET = 'packet';
+    public const GENERIC_PACKET = 'generic_packet';
+    public const DISCONNECT = 'disconnect';
+
+    private ClientState $state = ClientState::HANDSHAKING;
 
     public function __construct(
-        protected readonly ConnectionInterface $connection
+        protected readonly ConnectionInterface $connection,
+        public readonly Server $server
     ) {
         $this->setupEvents();
     }
@@ -21,10 +30,41 @@ class Client extends EventEmitter
         return $this->connection->getRemoteAddress();
     }
 
+    public function getState(): ClientState
+    {
+        return $this->state;
+    }
+
     private function setupEvents(): void
     {
         $this->connection->on('data', function ($data) {
-            $this->emit('data', [$data]);
+            $this->emit(self::RAW_DATA, [$data]);
         });
+
+        $this->connection->on('close', fn () => $this->emit(self::DISCONNECT));
+
+        /** @var ?GenericPacket $packet */
+        $packet = null;
+
+        $this->on(self::RAW_DATA, function ($data) use (&$packet) {
+            $packet = new GenericPacket($this);
+
+            $packet->write($data);
+
+            if ($packet->isReady()) {
+                $this->emit(self::GENERIC_PACKET, [$packet]);
+                $packet = null;
+            }
+        });
+    }
+
+    public function emit($event, array $arguments = []): void
+    {
+        parent::emit($event, [...$arguments, $this]);
+    }
+
+    public function disconnect(): void
+    {
+        $this->connection->close();
     }
 }
